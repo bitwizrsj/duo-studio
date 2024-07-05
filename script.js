c912c1bbe46604aac5cc27446e33973886e93174
Rohitsj627 <158316834+bitwizrsj@users.noreply.github.com>
2024-07-05 09:00:22 +0530

    page 3

--- script.js
+++ script.js
@@ -68,3 +68,18 @@ var tl2 = gsap.timeline({
 tl2.to(".main", {
     backgroundColor: "#fff"
 })
+
+var tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page1 h1",
+        scroller: ".main",
+        // markers:true,
+        start: "top -280%",
+        end: "top -300%",
+        scrub: 3
+    }
+})
+
+tl3.to(".main",{
+    backgroundColor:"#0F0D0D"
+})
